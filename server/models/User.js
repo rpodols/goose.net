85b389aef59bdb4340cbac3ef053646b1ab27046
Trey S <fjschmidt292@gmail.com>
2022-07-20 19:43:05 -0400

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,36 @@ const bcrypt = require('bcrypt');
 const Order = require('./Order');
 
 const userSchema = new Schema({
+    Username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    moderator: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 69;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
 });
 
 module.exports = User;
