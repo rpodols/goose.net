3000c8b7a8adb9638a0cc5bd8fb0d554e12d288a
ryanp <rdpodols@gmail.com>
2022-07-19 21:31:03 -0400

    initial commit of template server and client side code

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+
+const userSchema = new Schema({
+});
+
+module.exports = User;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models')
+
+module.exports = resolvers;
