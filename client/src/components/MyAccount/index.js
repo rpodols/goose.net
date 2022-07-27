720e4544ab0b1bfbee040cf96a6b1663fa5811dc
Trey S <fjschmidt292@gmail.com>
2022-07-27 19:03:09 -0400

    pushing for a pull on main

--- client/src/components/MyAccount/index.js
+++ client/src/components/MyAccount/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { useQuery, useMutation } from '@apollo/client'
+import { useParams } from 'react-router-dom'
 
 import { QUERY_USER, QUERY_ME, QUERY_SETLIST } from '../utils/queries'
-
+import { Setlist } from '../components/Setlist';
 
 const MyAccount = (props) => {
+    const { username: userParam } = useParams
+    const { data } = useQuery(userParam ? QUERY_USER : QUERY_ME , QUERY_SETLIST, {
+        variables: { username: userParam },
+    });
+
+    const user = data?.me || data?.user || {};
+
   return (
     <div>
       <div className="flex-row mb-3">
-        <h2 className="center">Your Account</h2>
-                
+        <h2 className="center">Your Account</h2>      
       </div>
-    </div>
+      <>
+          <Setlist
+            sets={user.sets} />
+        </>
+      </div>
   )
 }
 export default MyAccount;
