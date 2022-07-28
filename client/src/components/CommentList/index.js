0042b00eb7b02e5f3799cedf65ad1124be462f84
Trey S <fjschmidt292@gmail.com>
2022-07-28 19:33:53 -0400

    got comments jsx ready for styling

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -4,17 +4,19 @@ import React from 'react';
 const CommentList = ({ comments }) => {
     return (
         <div>
-            <div>
-                <span>Comments:</span>
-            </div>
-            <div>
+          <div className="comment-container">
+            <div className="comment-header">
+              <h4 className="comment-stamp">Comments:</h4>
+              <div>
                 {comments && comments.map(comment => (
-                    <p key={comment._id}>
-                        {comment.commentBody} 
-                        {comment.username} on {comment.createdAt}
+                    <p className="comment-content" key={comment._id}>
+                      {comment.commentBody} <br/> 
+                      {comment.username} on {comment.createdAt}
                     </p>
                 ))}
+              </div>
             </div>
+          </div>
         </div>
     );
 };
