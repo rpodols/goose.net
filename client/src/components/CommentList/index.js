305ccf4f605bbcc714f5d264719a472736d6ee35
Trey S <fjschmidt292@gmail.com>
2022-07-28 20:37:16 -0400

    fixed setlist displayed on other pages

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -3,22 +3,22 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
     return (
-        <div>
-          <div className="comment-container">
-            <div className="comment-header">
-              <h4 className="comment-stamp">Comments:</h4>
-              <div>
-                {comments && comments.map(comment => (
-                    <p className="comment-content" key={comment._id}>
-                      {comment.commentBody} <br/> 
-                      {comment.username} on {comment.createdAt}
-                    </p>
-                ))}
-              </div>
-            </div>
+    <div id='comment-area'>
+      <div className="comment-container">
+        <div className="comment-header">
+          <h4 className="comment-stamp">Comments:</h4>
+            <div>
+            {comments && comments.map(comment => (
+              <p className="comment-content" key={comment._id}>
+                {comment.commentBody} <br/> 
+                {comment.username} on {comment.createdAt}
+              </p>
+            ))}
           </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CommentList;
