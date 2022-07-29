ea5ea66b56f8453d96e53c12d20c3ead1e53043b
Trey S <fjschmidt292@gmail.com>
2022-07-28 22:10:12 -0400

    comments

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -7,15 +7,18 @@ const CommentList = ({ comments }) => {
       <div className="comment-container">
         <div className="comment-header">
           <h4 className="comment-stamp">Comments:</h4>
-            <div>
+        </div>
+            <div className="comment-content">
             {comments && comments.map(comment => (
-              <p className="comment-content" key={comment._id}>
-                {comment.commentBody} <br/> 
-                {comment.username} on {comment.createdAt}
-              </p>
+              <><p className="comment-content" key={comment._id}>
+                    {comment.commentBody} 
+                    </p>
+                    <p className="comment-user">
+                        {comment.username} on {comment.createdAt}
+                    </p></>
             ))}
           </div>
-        </div>
+        
       </div>
     </div>
   );
