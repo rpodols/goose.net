b81aac5afb714bf70bcad3f160c731ad0660e8c2
Trey S <fjschmidt292@gmail.com>
2022-07-29 09:36:46 -0400

    fixed header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -18,15 +18,22 @@ const Header = () => {
                     <div className="navigation">
                     {loggedIn ? (
                         <>
+                          <div className="main-icon">
                             <Link className="nav-bar" to="/">goose.vet</Link>
+                          </div>
                             <Link className="nav-bar" to="/myaccount">My Account</Link>
                             <a className="nav-bar" href="/" onClick={logout}>Logout</a>
                         </>
                     ) : (
                         <>
+                          <div className="main-icon">
                             <Link className="nav-bar" to="/">goose.net</Link>
+                          </div>
+                          <div className="nav-container">
                             <Link className="nav-bar" to="/login">Login</Link>
                             <Link className="nav-bar" to="/register">Register</Link>
+                          </div>
+                          
                         </>
                     )}
                     </div>
