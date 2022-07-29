a59f683152f192650e12500b62950754b1c5f3bd
Trey S <fjschmidt292@gmail.com>
2022-07-29 09:44:00 -0400

    applying header to logged in state and fixing  wrapping

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -19,10 +19,12 @@ const Header = () => {
                     {loggedIn ? (
                         <>
                           <div className="main-icon">
-                            <Link className="nav-bar" to="/">goose.vet</Link>
+                            <Link className="nav-bar" to="/">goose.net</Link>
                           </div>
+                          <div className="nav-container">
                             <Link className="nav-bar" to="/myaccount">My Account</Link>
-                            <a className="nav-bar" href="/" onClick={logout}>Logout</a>
+                            <Link className="nav-bar" to="/register">Register</Link>
+                          </div>
                         </>
                     ) : (
                         <>
